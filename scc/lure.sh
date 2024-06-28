4702eadf39b0a00e53569c3ac9278284451051c2
zocker-160 <zocker1600@posteo.net>
2024-06-28 18:02:32 +0200

    build scc from source

--- scc/lure.sh
+++ scc/lure.sh
@@ -1,19 +1,25 @@
 maintainer="zocker_160 <zocker1600 at posteo dot net>"
 
-name=scc-bin
-_name=scc
+name=scc
 version=3.3.4
 release=1
 desc="Sloc, Cloc and Code: scc is a very fast accurate code counter with complexity calculations and COCOMO estimates written in pure Go"
 homepage="https://github.com/boyter/scc"
-architectures=("amd64")
+architectures=("all")
 licenses=("MIT")
 
-sources=("https://github.com/boyter/scc/releases/download/v$version/scc_Linux_x86_64.tar.gz")
-checksums=('b3aa98dda0461c159f3e977b0408cba2c3d39aedd875b6e89c8280d0c6a208fb')
+#build_deps=("go-bin")
+
+sources=("git+https://github.com/boyter/scc.git?~rev=v$version")
+checksums=('SKIP')
+
+build() {
+	cd "$srcdir/$name"
+	go build
+}
 
 package() {
-	cd "$srcdir/scc_Linux_x86_64"
+	cd "$srcdir/$name"
 
 	install-binary scc
 	install-license LICENSE
