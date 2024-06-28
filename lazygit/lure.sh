0bbdab320ae35f68d0e5f48aee9d0a3a84957a88
zocker-160 <zocker1600@posteo.net>
2024-06-28 17:57:54 +0200

    add lazygit

---
+++ lazygit/lure.sh
@@ -0,0 +1,26 @@
+maintainer="zocker_160 <zocker1600 at posteo dot net>"
+
+name=lazygit
+version=0.40.2
+release=1
+desc="simple terminal UI for git commands"
+homepage="https://github.com/jesseduffield/lazygit"
+architectures=("all")
+licenses=("MIT")
+
+#build_deps=("go-bin")
+
+sources=("git+https://github.com/jesseduffield/lazygit.git?~rev=v$version")
+checksums=('SKIP')
+
+build() {
+	cd "$srcdir/$name"
+	go build
+}
+
+package() {
+	cd "$srcdir/$name"
+
+	install-binary lazygit
+	install-license LICENSE
+}
