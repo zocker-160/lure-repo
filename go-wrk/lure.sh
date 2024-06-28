938b64edc07588aba978b302f7df3dd42ddd5536
zocker-160 <zocker1600@posteo.net>
2024-06-28 17:50:55 +0200

    add go-wrk

---
+++ go-wrk/lure.sh
@@ -0,0 +1,31 @@
+maintainer="zocker_160 <zocker1600 at posteo dot net>"
+
+name=go-wrk
+version=1.0
+release=1
+desc="a HTTP benchmarking tool based in spirit on the excellent wrk tool "
+homepage="https://github.com/tsliwowicz/go-wrk"
+architectures=("all")
+licenses=("Apache-2.0")
+
+#build_deps=("go-bin")
+sources=("git+https://github.com/tsliwowicz/go-wrk.git")
+checksums=('SKIP')
+
+
+version() {
+	cd "$srcdir/$name"
+	printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
+}
+
+build() {
+	cd "$srcdir/$name"
+	go build
+}
+
+package() {
+	cd "$srcdir/$name"
+
+	install-binary go-wrk
+	install-license LICENSE
+}
