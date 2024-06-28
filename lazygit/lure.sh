e24273a922e189d4aa736c4b601c4cc61a301e7c
zocker-160 <zocker1600@posteo.net>
2024-06-28 18:20:22 +0200

    switch back to provided binary for lazygit

--- lazygit/lure.sh
+++ lazygit/lure.sh
@@ -5,20 +5,15 @@ version=0.40.2
 release=1
 desc="simple terminal UI for git commands"
 homepage="https://github.com/jesseduffield/lazygit"
-architectures=("all")
+architectures=("amd64")
 licenses=("MIT")
 
 #build_deps=("go-bin")
 
-sources=("git+https://github.com/jesseduffield/lazygit.git?~rev=v$version")
+sources=("https://github.com/jesseduffield/lazygit/releases/download/v${version}/lazygit_${version}_Linux_x86_64.tar.gz")
 checksums=('SKIP')
 
-build() {
-	cd "$srcdir/$name"
-	go build
-}
-
 package() {
-	cd "$srcdir/$name"
+	cd "$srcdir/lazygit_${version}_Linux_x86_64"
 	install-binary lazygit
 }
