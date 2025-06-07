e3b91e4ba868c90ed3d00ba0647b049b9b51ce61
zocker-160 <zocker1600@posteo.net>
2025-06-07 17:35:37 +0200

    update lazygit to 0.52.0

--- lazygit/lure.sh
+++ lazygit/lure.sh
@@ -1,7 +1,7 @@
 maintainer="zocker_160 <zocker1600 at posteo dot net>"
 
 name=lazygit
-version=0.40.2
+version=0.52.0
 release=1
 desc="simple terminal UI for git commands"
 homepage="https://github.com/jesseduffield/lazygit"
@@ -11,7 +11,7 @@ licenses=("MIT")
 #build_deps=("go-bin")
 
 sources=("https://github.com/jesseduffield/lazygit/releases/download/v${version}/lazygit_${version}_Linux_x86_64.tar.gz")
-checksums=('SKIP')
+checksums=('sha256:ca48006c41c94f389127fa67c29a866833c1aff9143eb5238939029d04404832')
 
 package() {
 	cd "$srcdir/lazygit_${version}_Linux_x86_64"
